ded25bace71616678bca3959cd7f7f234a569233
Yoreni <yoreni@icloud.com>
2023-03-22 11:00:51 +0100

    added 14 bag

--- src/game/PieceGeneration.js
+++ src/game/PieceGeneration.js
@@ -4,3 +4,10 @@ function sevenBag(currentQueue)
     shuffle(bag);
     return bag;
 }
+
+function fourteenBag(currentQueue)
+{
+    let bag = Object.keys(pieces).concat(Object.keys(pieces));
+    shuffle(bag);
+    return bag;
+}
