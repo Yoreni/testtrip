44ff71bc63b1a8522e3607e296964283b8de9135
Yoreni <yoreni@icloud.com>
2023-03-24 00:18:41 +0100

    added key listeners

--- src/Game.js
+++ src/Game.js
@@ -5,6 +5,16 @@ class Game extends IScene
         super();
         this.container = new PIXI.Container();
         this._objects = {};
+        this._keybaord = {
+            left: new KeyDetector(app, "ArrowLeft"),
+            right: new KeyDetector(app, "ArrowRight"),
+            softDrop: new KeyDetector(app, "ArrowDown"),
+            rotateClockwise: new KeyDetector(app, "ArrowUp"),
+            rotateAnticlockwise: new KeyDetector(app, "z"),
+            hold: new KeyDetector(app, "x"),
+            rotate180: new KeyDetector(app, "c"),
+            hardDrop: new KeyDetector(app, " "),
+        }
     }
 
     start()
@@ -19,7 +29,7 @@ class Game extends IScene
 
     update()
     {
-
+        this._objects.debugText.text = this._keybaord.hardDrop.getSecondsDown()
     }
 
     destory()
@@ -29,6 +39,8 @@ class Game extends IScene
 
     init()
     {
-  
+        this._objects.debugText = new PIXI.Text("", {fontSize: 24,fontWeight: "bold",fontFamily: "Calibri"});
+        this._objects.debugText.position.set(10, 50);
+        this.container.addChild(this._objects.debugText);
     }
 }
