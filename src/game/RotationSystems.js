1e97673cc1e52dae624f35dddbc7f451db589a4c
Yoreni <yoreni@icloud.com>
2023-04-26 15:07:18 +0200

    added the SRS+ kicktable object

--- src/game/RotationSystems.js
+++ src/game/RotationSystems.js
@@ -21,6 +21,20 @@ const SRSkicktable = {  //this could be simplified
     }
 }
 
+const SRSPlusKicktable = {  
+    "*": SRSkicktable["*"], //SRS+ is only difernt for the I Piece
+    "I": {
+        "01": [Point(0, 0), Point(1, 0), Point(-2, 0), Point(-2, -1), Point(1, 2)],
+        "10": [Point(0, 0), Point(-1, 0), Point(2, 0), Point(-1, -2), Point(2, 1)],
+        "12": [Point(0, 0), Point(-1, 0), Point(2, 0), Point(-1, 2), Point(2, -1)],
+        "21": [Point(0, 0), Point(-2, 0), Point(1, 0), Point(-2, 1), Point(1, -2)],
+        "23": [Point(0, 0), Point(2, 0), Point(-1, 0), Point(2, 1), Point(-1, -2)],
+        "32": [Point(0, 0), Point(1, 0), Point(-2, 0), Point(1, 2), Point(-2, -1)],
+        "30": [Point(0, 0), Point(1, 0), Point(-2, 0), Point(1, -2), Point(-2, 1)],
+        "03": [Point(0, 0), Point(-1, 0), Point(2, 0), Point(2, -1), Point(-1, 2)],
+    }
+}
+
 function SRS(direction, fallingPiece, playfield)
 {
     const kicktableType = fallingPiece.type === "I" ? "I" : "*";
