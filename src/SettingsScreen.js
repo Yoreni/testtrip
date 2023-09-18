08a640f00e7469b06dbf905d4c765590d48cefdc
Yoreni <yoreni@icloud.com>
2023-09-18 20:58:58 +0100

    added handling sliders

--- src/SettingsScreen.js
+++ src/SettingsScreen.js
@@ -12,9 +12,44 @@ class SettingsScreen extends IScene
         this._objects.back.onClick = () => sceneManager.start("modeMenu");
         this.container.addChild(this._objects.back)
 
-        this._objects.dasSlider = new Slider({width: 200, height: 20, label: "test"});
+        const dimensions = {width: 200, height: 15}
+
+        this._objects.dasSlider = new Slider({
+            ...dimensions,
+            label: "DAS", 
+            min: 17, 
+            max: 333, 
+            step: 16,
+            valueDisplay: (value) => `${value}ms`,
+            whileChanging: (value) => handling.DAS = value,
+        });
         this._objects.dasSlider.position.set(100, 100);
         this.container.addChild(this._objects.dasSlider)
+
+        this._objects.arrSlider = new Slider({
+            ...dimensions, 
+            label: "ARR", 
+            min: 0, 
+            max: 83,
+            step: 1,
+            valueDisplay: (value) => `${value}ms`,
+            whileChanging: (value) => handling.ARR = value,
+        });
+        this._objects.arrSlider.position.set(100, 150);
+        this.container.addChild(this._objects.arrSlider)
+
+        this._objects.sdfSlider = new Slider({
+            width: 200,
+            height: 15, 
+            label: "ARR", 
+            min: 5, 
+            max: 41,
+            step: 1,
+            valueDisplay: (value) => `x${value === 41 ? "∞" : value}`,
+            whileChanging: (value) => handling.SDF = value === 41 ? 1.79e308 : value,
+        });
+        this._objects.sdfSlider.position.set(100, 200);
+        this.container.addChild(this._objects.sdfSlider)
     }
 
     start()
